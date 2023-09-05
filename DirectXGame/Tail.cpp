2c53bb2c94cb2c288c85c4139afc44b099d09219
k022g0060@g.neec.ac.jp <k022g0060@g.neec.ac.jp>
2023-09-05 23:30:01 +0900

    弾の出方調整中

--- DirectXGame/Tail.cpp
+++ DirectXGame/Tail.cpp
@@ -95,8 +95,8 @@ void Tail::Fire() {
 	if (isFire_) {
 		// 尻尾の進む向きから弾を打ち出す向きを計算
 		Vector2 tailDirection = GetDirection();
-		Vector2 bulletDirection{-tailDirection.x, tailDirection.y};
-
+		Vector2 bulletDirection{tailDirection.x, tailDirection.y};
+		
 		PlayerBullet* newBullet = new PlayerBullet();
 		newBullet->Initialize(player_->GetBulletTex(), *GetPosition(), bulletDirection);
 		player_->AddBullets(newBullet);
